459ae056b6e7552f4e77956cd288b6fb5171720f
yutaroyutaro <yutaroyutaro172@gmail.com>
2018-08-20 15:38:00 +0900

    first commit

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//phpinfo();
+
+$test = 10;
+
+echo $test;
+
+$arrayTest = [1, 2, 3];
+
+foreach ($arrayTest as $value){
+    echo $value;
+}
+
+$test = 20;
+
+//if(!isset($_SESSION)){
+//    session_start();
+//}
+//
+//$_SESSION['test'] = $test;
+//
+//echo $_SESSION['test'];
